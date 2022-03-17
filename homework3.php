52efb5e50fd7c86ae397f6f1a80ddb95a210412c
Oscar <tywu.oscar@gmail.com>
2022-03-16 22:33:01 -0400

    Added calculateGrade()

--- homework3.php
+++ homework3.php
@@ -9,3 +9,28 @@
     // Your functions here
 
     // No closing php tag needed since there is only PHP in this file
+    function calculateGrade($scores, $drop){
+        $lowestPercentage = PHP_INT_MAX;
+        $lowestScored = 0;
+        $lowestAvail = 0;
+        $sumScored = 0;
+        $sumAvail = 0;
+        for($i = 0; $i < count($scores); $i++){
+            $percentage = round($scores["score"]/$scores["max"], 3);
+            if($percentage < $lowestPercentage){
+                $lowestScored = $scores["score"];
+                $lowsestAvail = $scores["max"];
+            }
+            $sumScored = $sumScored + $scores["score"];
+            $sumAvail = $sumAvail + $scores["max"];
+        }
+        if($drop){
+            $sumScored = $sumScored - $lowestScored;
+            $sumAvail = $sumAvail - $lowestAvail;
+            $average = round(100 * ($sumScored/$sumAvail), 3);
+            return $average;
+        }else{
+            $average = round(100 * ($sumScored/$sumAvail), 3);
+            return $average;
+        }
+    }
